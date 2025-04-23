7684a870b1fd72fc00edef651e5f14aeaf533e33
Ameleus <mertmocan0@gmail.com>
2025-04-23 23:05:50 +0300

    Update Version.cs

--- Version.cs
+++ Version.cs
@@ -1,6 +1,6 @@
 {
     "SoftwareVersion": "1.0",
-    "SoftwareUrl":"",
+    "SoftwareUrl":"https://www.dropbox.com/scl/fi/88bzpnos89zpytpk8d9ve/KurtExecutor.zip?rlkey=9dxe4oe96mlg8yxi6we7b3frm&st=jv12u7i2&dl=1",
     "Status":"true",
-    "Changelog":"[+] Build Start"
+    "Changelog":"[+] Kurt Executer Release"
 }
