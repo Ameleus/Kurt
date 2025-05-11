bcaa51007fd634981a0bc6af642ae0052c6e8c2a
Ameleus <mertmocan0@gmail.com>
2025-05-11 10:44:58 +0300

    Update del old link Version.cs

--- Version.cs
+++ Version.cs
@@ -1,6 +1,6 @@
 {
     "SoftwareVersion": "1.0",
-    "SoftwareUrl":"https://www.dropbox.com/scl/fi/88bzpnos89zpytpk8d9ve/KurtExecutor.zip?rlkey=9dxe4oe96mlg8yxi6we7b3frm&st=jv12u7i2&dl=1",
+    "SoftwareUrl":"",
     "Status":"true",
     "Changelog":"[+] New ui design",
     "PayStatus": "True"
